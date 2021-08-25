20bc3d3e9e4baa3bb1a738b7d885c0a5b626e34e
gstepenko <g.stepenko@rusbitech.ru>
2021-08-25 16:50:20 +0300

    Небольшие изменения

--- create-exercise.php
+++ create-exercise.php
@@ -13,9 +13,10 @@
   <div class="form-inner">
 	<h3>Выберите тип написания букв</h3>
 		<p>серый <input type="radio" name="type_letter_propisi" value=0 checked></p>
-		<p>пунктирный <input type="radio" name="type_letter_propisi" value=1></p>  
+		<p>пунктирный <input type="radio" name="type_letter_propisi" value=1></p>
     <h3>Место для прописи:</h3>
-	<textarea name="data1"rows="10"></textarea>
+	<textarea name="data1"rows="10">
+    </textarea>
     <input type="submit" value="Создать пропись">
   </div>
 </form>
